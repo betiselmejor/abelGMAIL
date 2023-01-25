e2449006f1eaa5d7340633afe2d465bec80db12c
aochoa <aochoa21@jaumebalmes.net>
2023-01-25 18:08:03 +0100

    inici

---
+++ app/src/main/java/com/example/mailabel/Detall.java
@@ -0,0 +1,14 @@
+package com.example.mailabel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Detall extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detall);
+    }
+}
